3cb21cf2000ecd63c2ac914bfe7a5746d01a844a
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-04-27 19:07:58 +0800

    finished first leson

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import "./App.css";
+import { TaskType, Todolist } from "./Todolist";
+
+let tasks1: Array<TaskType> = [
+  { id: 1, title: "CSS", isDone: true },
+  { id: 2, title: "JS", isDone: true },
+  { id: 3, title: "React", isDone: false },
+];
+
+let tasks2: Array<TaskType> = [
+  { id: 1, title: "Terminator", isDone: true },
+  { id: 2, title: "XXX", isDone: false },
+  { id: 3, title: "Jentlments of fortune", isDone: true },
+];
 
 function App() {
+  debugger;
   return (
     <div>
-      <TodoList />
+      <Todolist title="What to learn?" tasks={tasks1} />
+      <Todolist title="Movies" tasks={tasks2} />
     </div>
   );
-}
-function TodoList() {
-  debugger;
-  return <div>TodoList</div>;
 }
 
 export default App;

36b99c5d474151cdad4f726963ec876205a86f39
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-04-27 16:39:23 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
+function TodoList() {
+  debugger;
+  return <div>TodoList</div>;
+}
+
+export default App;
